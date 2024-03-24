8d0b56ff8670cda3c035385b8157e530e1c21b43
Samat Musaev <samkazus130293@gmail.com>
2024-03-24 16:52:20 +0600

    task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
