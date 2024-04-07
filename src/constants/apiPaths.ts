831e2864598522b43cbbfa9058c01b3bd514c753
Samat Musaev <smusaev@delhaize.be>
2024-04-07 22:20:05 +0600

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://6mwzoxcnaf.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://ji1e4k0oh0.execute-api.us-east-1.amazonaws.com/dev",
 };
